bc0f40eea1dd104fb0e292f7ca34bdae3c1f37d2
ДМ. Па <qtckpuhdsa@gmail.com>
2020-12-21 16:49:37 +0300

    -

--- check_folder.R
+++ check_folder.R
@@ -1,10 +1,11 @@
 
 source('abtest.r')
 
+path = str_c(getwd(), '/ration/op_before')
 
-files = list.files(path = getwd(), pattern = '.*\\.csv')
+files = list.files(path = path, pattern = '.*\\.csv')
 
-mats = lapply(files, function(fl) ab_test(read_csv(fl))[['mat']])
+mats = lapply(files, function(fl) ab_test(read_csv(str_c(path, '/', fl)))[['mat']])
 
 total_mat = Reduce("+", mats)/length(mats)
 
